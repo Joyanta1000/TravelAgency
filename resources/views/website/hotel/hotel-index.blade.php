2bf2c3326d105cebeed364cda27dfb9112e980f1
Joyanta Dutta <joyanta955@gmail.com>
2022-03-26 17:19:26 +0600

    name

--- resources/views/website/hotel/hotel-index.blade.php
+++ resources/views/website/hotel/hotel-index.blade.php
@@ -59,7 +59,7 @@
                     <li><a href="#flight-and-hotel-tab">FLIGHT &amp; HOTELS</a></li>
                     @auth
 
-                    <li><a href="#near-me">NEAR MyE</a></li>
+                    <li><a href="#near-me">NEAR ME</a></li>
 
                     @else
 
